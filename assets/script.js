318a87fde83f1e6132f568caa5839a744ab48e47
Alessia Daffredo <alessiadaffredo@gmail.com>
2025-06-06 15:59:20 +0200

    Added projects

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const menuIcon = document.querySelector(".menu-icon"); 
+  if(menuIcon) {
+    menuIcon.addEventListener("click", openNav);
+  }
+})
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
