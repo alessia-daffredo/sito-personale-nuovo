20fcf19dafe809c3680a0bc73c685975e93c4f98
Alessia Daffredo <alessiadaffredo@gmail.com>
2025-06-09 18:52:20 +0200

    Corretto errore js

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const menuIcon = document.querySelector(".menu-icon"); 
-  if(menuIcon) {
-    menuIcon.addEventListener("click", openNav);
-  }
-})
-
 function openNav() {
-  const sidenav =
-  document.querySelector(".sidenav");
+  const sidenav = document.querySelector(".sidenav");
   if (sidenav) {
-    sidenav.style.width = "250px"; 
+    sidenav.style.width = "250px";
   }
 }
 
 function closeNav() {
-  const sidenav = 
-  document.querySelector(".sidenav");
+  const sidenav = document.querySelector(".sidenav");
   if (sidenav) {
-    sidenav.style.width = "0"; 
+    sidenav.style.width = "0";
   }
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuIcon = document.querySelector(".menu-icon");
+  if (menuIcon) {
+    menuIcon.addEventListener("click", openNav);
+  }
+
+  const closeBtn = document.querySelector(".closebtn");
+  if (closeBtn) {
+    closeBtn.addEventListener("click", closeNav);
+  }
+
+  document.querySelectorAll(".sidenav a").forEach(link => {
+    link.addEventListener("click", closeNav);
+  });
+});
